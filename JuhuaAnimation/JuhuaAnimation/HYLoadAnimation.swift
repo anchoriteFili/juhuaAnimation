23d99d1a33f2d07c3de8bf66bdce729c539bc1eb
zetafin <feiliyaailiya1@ilcoud.com>
2017-12-13 17:07:09 +0800

    创建项目

---
+++ JuhuaAnimation/JuhuaAnimation/HYLoadAnimation.swift
@@ -0,0 +1,13 @@
+//
+//  HYLoadAnimation.swift
+//  JuhuaAnimation
+//
+//  Created by zetafin on 2017/12/13.
+//  Copyright © 2017年 赵宏亚. All rights reserved.
+//
+
+import UIKit
+
+class HYLoadAnimation: NSObject {
+
+}
